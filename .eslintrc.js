dd35583161e18e74aa46fe1f61dc61b1a603e0fe
Jonathan Coleman <jcoleman3307@gmail.com>
2016-10-31 10:36:14 -0400

    Add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "eslint-config-airbnb-es5",
+    "installedESLint": true,
+    "rules": {
+        "indent": ["error", 3],
+        "space-before-function-paren": ["error", "always"],
+        "func-names": 0
+    }
+};
